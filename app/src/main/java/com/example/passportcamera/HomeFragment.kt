9ea2183a9f2352d2175ba1d32d21a87e0af833f4
Varun Kataria <varunkataria@gmail.com>
2020-06-18 19:09:54 -0400

    Fixed bugs. Added navigation drawer.

---
+++ app/src/main/java/com/example/passportcamera/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.passportcamera
+
+import androidx.fragment.app.Fragment
+
+/**
+ * The home screen, which allows you to access the navigation drawer.
+ */
+
+class HomeFragment : Fragment() {
+
+
+}
